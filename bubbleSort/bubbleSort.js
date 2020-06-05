5cdf628e71cf5eb0bb48f50bed1fe76001651343
Kabalisa <ikabalisa20@gmail.com>
2020-06-05 21:44:51 +0200

    bubble sort

---
+++ bubbleSort/bubbleSort.js
@@ -0,0 +1,21 @@
+const swap = (array, idx1, idx2) => {
+  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+  return array;
+};
+let noSwap;
+const bubbleSort = (arr) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    noSwap = true;
+    for (let j = 0; j <= i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return arr;
+};
+
+console.log(bubbleSort([6, 1, 2, 3, 4, 5]));
