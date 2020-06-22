6b30f1e1eccac009f7f364344cd91929cf53a7d2
Kabalisa <ikabalisa20@gmail.com>
2020-06-22 22:37:39 +0200

    tree traversal

--- dataStructures/binarySearchTree/binarySearchTree.js
+++ dataStructures/binarySearchTree/binarySearchTree.js
@@ -131,13 +131,3 @@ class BinarySearchTree {
   }
 }
 
-let tree = new BinarySearchTree();
-
-tree.insert(10);
-tree.insert(6);
-tree.insert(3);
-tree.insert(8);
-tree.insert(15);
-tree.insert(20);
-
-console.log(tree.dfsInOrder());
