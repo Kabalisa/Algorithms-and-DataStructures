1078c898eafba667fcbc7193279d2f23f48be622
Kabalisa <ikabalisa20@gmail.com>
2020-07-02 13:46:09 +0200

    dijkstraShortPathAlgorith

--- dataStructures/graphs/weightedGraph.js
+++ dataStructures/graphs/weightedGraph.js
@@ -1,12 +1,145 @@
 class WeightedGraph {
-    constructor() {
-        this.adjacencyList = {};
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+
+  dijkstraShortPath(startVertex, endVertex) {
+    let distances = {};
+    let priorityQueue = new PriorityQueue();
+    let previous = {};
+    let path = [];
+
+    for (let element in this.adjacencyList) {
+      if (element === startVertex) {
+        distances[element] = 0;
+        priorityQueue.enqueue(element, 0);
+        previous[element] = null;
+      } else {
+        distances[element] = Infinity;
+        priorityQueue.enqueue(element, Infinity);
+        previous[element] = null;
+      }
     }
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+
+    while (priorityQueue.values.length) {
+      let vertex = priorityQueue.dequeue().val;
+
+      if (vertex === endVertex) {
+        while (previous[vertex]) {
+          path.push(vertex);
+          vertex = previous[vertex];
+        }
+        break;
+      }
+      if (vertex || distances[vertex] !== Infinity) {
+        for (let element of this.adjacencyList[vertex]) {
+          let distance = distances[vertex] + element.weight;
+          if (distance < distances[element.node]) {
+            let Element = element.node;
+            distances[Element] = distance;
+            previous[Element] = vertex;
+            priorityQueue.enqueue(Element, distance);
+          }
+        }
+      }
     }
-    addEdge(vertex1,vertex2, weight){
-        this.adjacencyList[vertex1].push({node:vertex2,weight});
-        this.adjacencyList[vertex2].push({node:vertex1, weight});
-    }
+    return path.concat("A").reverse();
+  }
 }
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+let graph = new WeightedGraph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
+
+// console.log(graph.adjacencyList);
+console.log(graph.dijkstraShortPath("A", "E"));
